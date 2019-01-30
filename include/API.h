6c14ead3db44d726cd505c5b4d416a9f831fd04e
acetousk <acetousk@gmail.com>
2019-01-29 21:37:29 -0700

    middle of PID

--- include/API.h
+++ include/API.h
@@ -282,9 +282,154 @@ void punch() {
     stopTask(puncherOff);
 }
 
+// PID using optical shaft encoder
+//
+// Shaft encoder has 360 pulses per revolution
+//
+
+#define PID_SENSOR_SCALE    1
+
+#define PID_MOTOR_SCALE     -1
+
+#define PID_INTEGRAL_LIMIT  50
+
+// These could be constants but leaving
+// as variables allows them to be modified in the debugger "live"
+float  pid_Kp = 2.0;
+float  pid_Ki = 0.0;
+float  pid_Kd = 0.0;
+
+static int   pidRunning = 1;
+static float pidRequestedValue;
+
+/*-----------------------------------------------------------------------------*/
+/*                                                                             */
+/*  pid control task                                                           */
+/*                                                                             */
+/*-----------------------------------------------------------------------------*/
+
+task pidPos()
+{
+    float  pidSensorCurrentValue;
+
+    float  pidError;
+    float  pidLastError;
+    float  pidIntegral;
+    float  pidDerivative;
+    float  pidDrive;
+
+    // If we are using an encoder then clear it
+    if( SensorType[ leftEncoder ] == sensorQuadEncoder )
+        SensorValue[ leftEncoder ] = 0;
+
+    if( SensorType[ rightEncoder ] == sensorQuadEncoder )
+        SensorValue[ rightEncoder ] = 0;
+
+    // Init the variables - thanks Glenn :)
+    pidLastError  = 0;
+    pidIntegral   = 0;
+
+    while( true )
+        {
+        // Is PID control active ?
+        if( pidRunning )
+            {
+            // Read the sensor value and scale
+            pidSensorCurrentValue = SensorValue[ leftEncoder ];
+
+            // calculate error
+            pidError = pidSensorCurrentValue - pidRequestedValue;
+
+            // integral - if Ki is not 0
+            if( pid_Ki != 0 )
+                {
+                // If we are inside controlable window then integrate the error
+                if( abs(pidError) < PID_INTEGRAL_LIMIT )
+                    pidIntegral = pidIntegral + pidError;
+                else
+                    pidIntegral = 0;
+                }
+            else
+                pidIntegral = 0;
+
+            // calculate the derivative
+            pidDerivative = pidError - pidLastError;
+            pidLastError  = pidError;
+
+            // calculate drive
+            pidDrive = (pid_Kp * pidError) + (pid_Ki * pidIntegral) + (pid_Kd * pidDerivative);
+
+            // limit drive
+            if( pidDrive > 90 )
+                pidDrive = 90;
+            if( pidDrive < -90 )
+                pidDrive = -90;
+
+            // send to motor
+            motor[left1] = pidDrive;
+            motor[left2] = pidDrive;
+            motor[left3] = pidDrive;
+
+            motor[right1] = pidDrive;
+            motor[right2] = pidDrive;
+            motor[right3] = pidDrive;
+            }
+        else
+            {
+            // clear all
+            pidError      = 0;
+            pidLastError  = 0;
+            pidIntegral   = 0;
+            pidDerivative = 0;
+
+            // send to motor
+            motor[left1] = 0;
+            motor[left2] = 0;
+            motor[left3] = 0;
+
+            motor[right1] = 0;
+            motor[right2] = 0;
+            motor[right3] = 0;
+            }
+
+        // Run at 50Hz
+        wait1Msec( 25 );
+        }
+}
+
+/*-----------------------------------------------------------------------------*/
+/*                                                                             */
+/*  main task                                                                  */
+/*                                                                             */
+/*-----------------------------------------------------------------------------*/
+
+void pidPosReq(int val)
+{
+    // send the motor off somewhere
+    pidRequestedValue = val;
+
+    // start the PID task
+    startTask( pidPos );
+
+    // use joystick to modify the requested position
+    while( true )
+        {
+        // maximum change for pidRequestedValue will be 127/4*20, around 640 counts per second
+        // free spinning motor is 100rmp so 1.67 rotations per second
+        // 1.67 * 360 counts is 600
+
+        pidRequestedValue = pidRequestedValue + (vexRT[ Ch2 ]/4);
+
+        wait1Msec(50);
+        }
+
+}
+
 void auton(){
     punch();
-    startTask(intakeOn);  
+    startTask(intakeOn);
+    pidPosReq(1100);
+
 }
 
 /*********************************************************************/
